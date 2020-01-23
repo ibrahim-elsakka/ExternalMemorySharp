f793567e6f5dd01a2631e6d387910f1c826fe153
CorrM <Islamnofl.official@gmail.com>
2020-01-23 04:11:45 +0200

    #

--- ExternalMemorySharp/Helper/HeapHelper.cs
+++ ExternalMemorySharp/Helper/HeapHelper.cs
@@ -120,8 +120,6 @@ namespace ExternalMemory.Helper
             }
 
             #region IDisposable Support
-            private bool disposedValue = false; // To detect redundant calls
-
             protected virtual void Dispose(bool disposing)
             {
                 if (!disposedValue)
